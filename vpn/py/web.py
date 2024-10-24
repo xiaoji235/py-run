de7af9cca1ad3da3c08ede121aa74e70a46a55a1
乐π <73387366+xiaoji235@users.noreply.github.com>
2024-10-25 07:12:48 +0800

    Add files via upload

---
+++ vpn/py/web.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://blog.banyunxiaoxi.icu'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1'
+}
+
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+first_title = soup.find_all(class_='title')[1].a['href']
+#print(first_title)
+new_url = first_title
+response = requests.get(new_url, headers=headers)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+blockquote = soup.find('blockquote')
+if blockquote:
+    p = blockquote.find('p')
+    if p:
+        blockquote_content = p.get_text().replace('<br />', '')
+        print(blockquote_content)
+    else:
+        print("")
+else:
+    print("")
